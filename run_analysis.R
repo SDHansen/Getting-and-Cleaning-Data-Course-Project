018d54e8d4c51bb47e6385f33ca3a1944795dff3
Scott Hansen <scotthansen655@gmail.com>
2014-12-19 15:38:18 -0700

    Let's how that this is the final change.

--- run_analysis.R
+++ run_analysis.R
@@ -7,35 +7,31 @@
 ##    5.  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 ##  It is based on the work done by Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto at Smartlab - Non Linear Complex Systems Laboratory.
 ##  The data and information is available at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones#
-##  It assumes that you have already downloaded and unzipped the data into sub-directory called data
  
 
 ## Programatically load multiple Libraries
 toLoad<-list("reshape2", "data.table")
 invisible(lapply(toLoad, library, character.only=T))
 
-## Set project directory.  Can be commented out or updated to reflect location of files
-setwd("F:\\Data Scientist\\Data Science Track\\Getting and Cleaning Data\\ClassProject")
-
 ##  Load file meta-data
 
-activity_labels<-read.table("./data/activity_labels.txt", col.names=c("activity_id", "activity_label"))
+activity_labels<-read.table("./activity_labels.txt", col.names=c("activity_id", "activity_label"))
 
-features<-read.table("./data/features.txt")
+features<-read.table("./features.txt")
 features<-features[,2]
 
 ##  Load Test data
 
-X_test<-read.table("./data/test/X_test.txt", col.names = features)
-subject<-read.table("./data/test/subject_test.txt", col.names="subject")
-activity_id<-read.table("./data/test/y_test.txt", col.names="activity_id")
+X_test<-read.table("./test/X_test.txt", col.names = features)
+subject<-read.table("./test/subject_test.txt", col.names="subject")
+activity_id<-read.table("./test/y_test.txt", col.names="activity_id")
 test_data<-cbind(subject, activity_id, X_test)
 
 ## Load Train data
 
-X_train<-read.table("./data/train/X_train.txt", col.names = features)
-subject<-read.table("./data/train/subject_train.txt", col.names="subject")
-activity_id<-read.table("./data/train/y_train.txt", col.names="activity_id")
+X_train<-read.table("./train/X_train.txt", col.names = features)
+subject<-read.table("./train/subject_train.txt", col.names="subject")
+activity_id<-read.table("./train/y_train.txt", col.names="activity_id")
 train_data<-cbind(subject, activity_id, X_train)
 
 ## Combine both train and test into a single data source
